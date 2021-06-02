bce35318dae5f1484f859cacdc98d6fa3cb088d0
Mouyad <mouyad.gd@gmail.com>
2021-06-01 23:09:14 -0400

    secondtry

---
+++ logic.js
@@ -0,0 +1,8 @@
+n = new Date();
+y = n.getFullYear();
+m = n.getMonth() + 1;
+d = n.getDate();
+document.getElementById("date").innerHTML = m + "/" + d + "/" + y;
+document.getElementById("anotherDate").innerHTML = m + "/" + d + "/" + y;
+
+
